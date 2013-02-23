99a2a3b6d8b7e8a8d932e690b1406d5b096dcebe
Andrew McGlathery <a.mcglathery@gmail.com>
2013-02-23 18:49:17 -0500

    added boostrap (for real)

--- hipnest/hipnest/settings.py
+++ hipnest/hipnest/settings.py
@@ -1,3 +1,6 @@
+import os
+ROOT_PATH = os.path.dirname(__file__)
+print ROOT_PATH
 # Django settings for hipnest project.
 
 DEBUG = True
@@ -56,11 +59,12 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(ROOT_PATH, 'static')
+print "STATIC_ROOT", STATIC_ROOT
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://localhost:8000/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -106,6 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(ROOT_PATH, 'templates')
 )
 
 INSTALLED_APPS = (
@@ -115,7 +120,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
