37eca4f6b72f6f5a7b5003b5299f028fb77d5f47
Griselle <griselle@ubuntu.(none)>
2013-02-23 22:54:01 -0500

    pushing some CSS

--- hipnest/hipnest/static/js/suggest.js
+++ hipnest/hipnest/static/js/suggest.js
@@ -5,14 +5,23 @@ var bands = ['Goldfish', 'RJD2', 'Queen']
 $('#searchbar').typeahead({source:bands})
 
 $(document).ready(function(){
+    var rating = 0.5
     $('#left_button').click(function(){
-	alert('hiiiii');
-	$('#a_id').hide("slide", {direction: "left"}, 1000, function(){
-	    $('#a_id').html("CHANGED!");
-	    $('#a_id').show("slide", {direction: "right"}, 1000);
+	rating = rating - 0.1;
+	$('#artist_info').hide("slide", {direction: "left"}, 1000, function(){
+	    $('#artist_info').html("CHANGED!  " + rating);
+	    $('#artist_info').show("slide", {direction: "right"}, 1000);
 	});
-	
-	
     });
+
+    $('#right_button').click(function(){
+	 rating = rating + 0.1;
+	$('#artist_info').hide("slide", {direction: "right"}, 1000, function(){
+	    $('#artist_info').html("CHANGED!  " + rating);
+	    $('#artist_info').show("slide", {direction: "left"}, 1000);
+	});
+		
+    });
+
     
 });
